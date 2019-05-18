705e5a146910f2cdbfd14acfea3c41e87804b014
kenji matsuoka <egs@firespeed.org>
2019-05-18 23:53:27 +0900

    Implement view finder.

--- app/src/main/java/org/firespeed/cameraxtutorial/MainActivity.kt
+++ app/src/main/java/org/firespeed/cameraxtutorial/MainActivity.kt
@@ -6,8 +6,14 @@ import android.Manifest
 import android.content.pm.PackageManager
 import android.util.Size
 import android.graphics.Matrix
+import android.util.Rational
+import android.view.Surface
 import android.view.TextureView
+import android.view.ViewGroup
 import android.widget.Toast
+import androidx.camera.core.CameraX
+import androidx.camera.core.Preview
+import androidx.camera.core.PreviewConfig
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
 import java.util.concurrent.TimeUnit
@@ -26,10 +32,38 @@ class MainActivity : AppCompatActivity(), LifecycleOwner {
     private lateinit var viewFinder: TextureView
 
     private fun startCamera(){
+        val previewConfig = PreviewConfig.Builder().apply{
+            setTargetAspectRatio(Rational(1,1))
+            setTargetResolution(Size(640,640))
+        }.build()
+        val preview = Preview(previewConfig)
+        preview.setOnPreviewOutputUpdateListener {
+            val parent = viewFinder.parent as ViewGroup
+            parent.removeView(viewFinder)
+            parent.addView(viewFinder, 0)
+
+            viewFinder.surfaceTexture = it.surfaceTexture
+            updateTransform()
+        }
+        CameraX.bindToLifecycle(this, preview)
 
     }
 
     private fun updateTransform(){
+        val matrix = Matrix()
+        val centerX = viewFinder.width / 2f
+        val centerY = viewFinder.height / 2f
+
+        val rotationDegrees = when(viewFinder.display.rotation){
+            Surface.ROTATION_0 -> 0
+            Surface.ROTATION_90 -> 9
+            Surface.ROTATION_180 ->180
+            Surface.ROTATION_270 -> 270
+            else -> return
+        }
+
+        matrix.postRotate(-rotationDegrees.toFloat(), centerX, centerY)
+        viewFinder.setTransform(matrix)
 
     }
 
