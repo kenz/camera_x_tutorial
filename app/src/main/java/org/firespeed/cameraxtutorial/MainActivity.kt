38620ca1cf6193b73fa96ec9c606a3f88f3b8f63
kenji matsuoka <egs@firespeed.org>
2019-05-20 23:28:51 +0900

    Implement view finder.

--- app/src/main/java/org/firespeed/cameraxtutorial/MainActivity.kt
+++ app/src/main/java/org/firespeed/cameraxtutorial/MainActivity.kt
@@ -14,6 +14,7 @@ import android.widget.Toast
 import androidx.camera.core.CameraX
 import androidx.camera.core.Preview
 import androidx.camera.core.PreviewConfig
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
 import java.util.concurrent.TimeUnit
@@ -27,14 +28,23 @@ class MainActivity : AppCompatActivity(), LifecycleOwner {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewFinder = findViewById(R.id.view_finder)
+        if (allPermissionsGranted()) {
+            viewFinder.post { startCamera() }
+        } else {
+            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSION)
+        }
+        viewFinder.addOnLayoutChangeListener { _, _, _, _, _, _, _, _, _ ->
+            updateTransform()
+        }
     }
 
     private lateinit var viewFinder: TextureView
 
-    private fun startCamera(){
-        val previewConfig = PreviewConfig.Builder().apply{
-            setTargetAspectRatio(Rational(1,1))
-            setTargetResolution(Size(640,640))
+    private fun startCamera() {
+        val previewConfig = PreviewConfig.Builder().apply {
+            setTargetAspectRatio(Rational(1, 1))
+            setTargetResolution(Size(640, 640))
         }.build()
         val preview = Preview(previewConfig)
         preview.setOnPreviewOutputUpdateListener {
@@ -49,15 +59,15 @@ class MainActivity : AppCompatActivity(), LifecycleOwner {
 
     }
 
-    private fun updateTransform(){
+    private fun updateTransform() {
         val matrix = Matrix()
         val centerX = viewFinder.width / 2f
         val centerY = viewFinder.height / 2f
 
-        val rotationDegrees = when(viewFinder.display.rotation){
+        val rotationDegrees = when (viewFinder.display.rotation) {
             Surface.ROTATION_0 -> 0
             Surface.ROTATION_90 -> 9
-            Surface.ROTATION_180 ->180
+            Surface.ROTATION_180 -> 180
             Surface.ROTATION_270 -> 270
             else -> return
         }
@@ -68,10 +78,10 @@ class MainActivity : AppCompatActivity(), LifecycleOwner {
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        if(requestCode == REQUEST_CODE_PERMISSION){
-            if(allPermissionsGranted()){
-                viewFinder.post{ startCamera()}
-            }else{
+        if (requestCode == REQUEST_CODE_PERMISSION) {
+            if (allPermissionsGranted()) {
+                viewFinder.post { startCamera() }
+            } else {
                 Toast.makeText(this, "Permissions not granted by the user.", Toast.LENGTH_SHORT).show()
                 finish()
             }
@@ -79,9 +89,9 @@ class MainActivity : AppCompatActivity(), LifecycleOwner {
         }
     }
 
-    private fun allPermissionsGranted():Boolean{
-        return REQUIRED_PERMISSIONS.all{
-            ContextCompat.checkSelfPermission(this,it) == PackageManager.PERMISSION_GRANTED
+    private fun allPermissionsGranted(): Boolean {
+        return REQUIRED_PERMISSIONS.all {
+            ContextCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED
         }
     }
 }
